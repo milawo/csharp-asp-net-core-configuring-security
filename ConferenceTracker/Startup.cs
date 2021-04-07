932de0d5d3137cd18aef2d6d51ff4db9537d8804
milawo <mlw@miwosolutions.dk>
2021-04-07 16:53:04 +0200

    step 1 - 6

--- ConferenceTracker/Startup.cs
+++ ConferenceTracker/Startup.cs
@@ -22,6 +22,7 @@ namespace ConferenceTracker
         public IConfiguration Configuration { get; }
         public string SecretMessage { get; set; }
 
+        private readonly string _allowedOrigins = "_allowedOrigins";
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -33,16 +34,37 @@ namespace ConferenceTracker
             services.AddRazorPages();
             services.AddTransient<IPresentationRepository, PresentationRepository>();
             services.AddTransient<ISpeakerRepository, SpeakerRepository>();
+            services.AddCors(options =>
+            {
+                options.AddPolicy(_allowedOrigins, builder =>
+                {
+                    builder.WithOrigins("http://pluralsight.com");
+                });
+            }); 
         }
 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {            
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+                
+            
+            
             using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
             using (var context = scope.ServiceProvider.GetService<ApplicationDbContext>())
                 context.Database.EnsureCreated();
-
+            app.UseCors(_allowedOrigins);
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
